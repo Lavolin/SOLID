7a20569c79f00e13f384fed448e61c1bcac54be0
Lavolin <lavolin@gmail.com>
2022-06-18 17:05:34 +0300

    18061705

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -6,7 +6,24 @@ namespace Logger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ILayout simpleLayout = new SimpleLayout();
+            IAppender consoleAppender = new ConsoleAppender(simpleLayout);
+            ILogger logger = new Logger(consoleAppender);
+
+            logger.Error("3/26/2015 2:08:11 PM", "Error parsing JSON.");
+            logger.Info("3/26/2015 2:08:11 PM", "User Pesho successfully registered.");
+
+
+            //var simpleLayout = new SimpleLayout();
+            //var consoleAppender = new ConsoleAppender(simpleLayout);
+
+            //var file = new LogFile();
+            //var fileAppender = new FileAppender(simpleLayout, file);
+
+            //var logger = new Logger(consoleAppender, fileAppender);
+            //logger.Error("3/26/2015 2:08:11 PM", "Error parsing JSON.");
+            //logger.Info("3/26/2015 2:08:11 PM", "User Pesho successfully registered.");
+
         }
     }
 }
